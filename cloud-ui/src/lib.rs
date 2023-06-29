152ef93adc5ebe7b5f7deac29384b3c9cc856c04
han <calm0406@163.com>
2023-06-29 11:34:01 +0000

    root

--- cloud-ui/src/lib.rs
+++ cloud-ui/src/lib.rs
@@ -3,5 +3,5 @@ include!(concat!(env!("OUT_DIR"), "/generated.rs"));
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     let generated = generate();
-    cfg.service(actix_web_static_files::ResourceFiles::new("/", generated).resolve_not_found_to("index.html"));
+    cfg.service(actix_web_static_files::ResourceFiles::new("/", generated).resolve_not_found_to_root());
 }

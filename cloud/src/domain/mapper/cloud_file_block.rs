8b841e458f746ecbdc9a20736ac7897673796f12
Calm0406 <calm0406@163.com>
2023-09-07 00:16:54 +0800

    dav opt

--- cloud/src/domain/mapper/cloud_file_block.rs
+++ cloud/src/domain/mapper/cloud_file_block.rs
@@ -5,7 +5,7 @@ crud!(CloudFileBlock {});
 impl_update!(CloudFileBlock{update_by_status(id: i32,status :i8) =>"` where id=#{id} and status=#{status}`"});
 
 impl CloudFileBlock {
-    #[sql("select cfb.* from cloud_file_block cfb join file_block_meta fbm on cfb.file_block_id = fbm.id where ((cfb.cloud_file_hash != fbm.part_hash) or cfb.cloud_file_hash is null) and cfb.status<>2 and cfb.status<>7 ")]
+    #[sql("select cfb.* from cloud_file_block cfb join file_block_meta fbm on cfb.file_block_id = fbm.id where cfb.status = 1")]
     pub(crate) async fn select_to_upload(rb: &mut RBatis) -> Result<Vec<CloudFileBlock>, Error> {
         impled!()
     }
